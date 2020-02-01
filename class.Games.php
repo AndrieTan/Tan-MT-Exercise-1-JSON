bda0536db9e4fc0893f70c614a39cec8b015431c
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-02-01 19:20:00 +0800

    Create class.Games.php

---
+++ class.Games.php
@@ -0,0 +1,24 @@
+<?php
+class Games{
+	public $db;
+	
+	public function __construct(){
+		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}else {
+		}
+	}
+	
+	public function get_Games(){
+		$sql = "SELECT * FROM tbl_games";
+		$result = mysqli_query($this->db,$sql);
+		while($row = mysqli_fetch_assoc($result)){
+			$list[] = $row;
+		}
+		return $list;
+	}
+	
+}
+?>
